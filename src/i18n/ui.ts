34ce719c44cf850dd08b9161feb933a0d14c440f
ifranferrandez <franciscoferrandezmartinez@gmail.com>
2024-09-17 14:27:43 +0200

    Langs

---
+++ src/i18n/ui.ts
@@ -0,0 +1,92 @@
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "about.welcome": "Welcome!",
+    "about.intro":
+      "I am a software engineer graduated from the University of Alicante, passionate about software development and process automation.",
+    "about.description":
+      "I consider myself a proactive, communicative, and curious person. I am looking to continue developing in the business world, seeking new challenges and enjoying teamwork projects.",
+    "about.experience":
+      "Although I have been practicing my profession for a short time, I have already created a variety of applications, including desktop solutions, web applications, REST API services, and projects that interact with external services.",
+    "about.projects":
+      "Below you can find detailed information about some of my skills and the projects I have participated in.",
+
+    "experience.title": "Experience",
+    "experience.smurfit.duration": "June 2022 - December 2022",
+    "experience.smurfit.description":
+      "IT support at the factory. Networks and shell scripting.",
+    "experience.akra.duration": "January 2023 - Present",
+    "experience.akra.description": "Application development.",
+    
+    "projects.title": "Projects",
+    "projects.sgiweb.title": "SGIWeb",
+    "projects.sgiweb.description":
+      "Operate your business with this ERP that offers a modern user interface and smooth user experience, supporting automation and management to allow businesses to maintain rigorous control over their daily operations.",
+    "projects.akraVeriFactu.title": "AkraVeriFactu",
+    "projects.akraVeriFactu.description":
+      "Facilitates compliance with tax regulations by providing a platform for the management and submission of tax documents. AkraVeriFactu offers a robust and reliable solution for businesses that need to stay up to date with tax regulations.",
+    "projects.akraFACe.title": "AkraFACe",
+    "projects.akraFACe.description":
+      "Manage your electronic invoices safely and efficiently. Facilitating the submission of invoices through an integrated application or from the FACe portal.",
+    "projects.mailManager.title": "MailManager",
+    "projects.mailManager.description":
+      "Automate the management of emails. This tool searches for unread emails in the inbox to generate automatic responses.",
+
+    'languages.title': 'Languages',
+    'languages.spanish.label': 'Spanish',
+    'languages.spanish.level': 'NATIVE',
+    'languages.english.label': 'English',
+    'languages.valencian.label': 'Valencian',
+
+    'technologies.title': 'Technologies',
+
+    'footer.rights': '© 2024 Francisco Ferrández Martínez. All rights reserved.'
+  },
+  es: {
+    "about.welcome": "¡Bienvenido!",
+    "about.intro":
+      "Soy un ingeniero informático graduado en la Universidad de Alicante apasionado por el desarrollo de software y la automatización de procesos.",
+    "about.description":
+      "Me considero una persona resolutiva, comunicativa e inquieta. Estoy buscando continuar mi desarrollo en el mundo empresarial para vivir nuevos retos y disfrutar de proyectos en equipo.",
+    "about.experience":
+      "Aunque llevo poco tiempo ejerciendo mi profesión, ya he creado una variedad de aplicaciones, incluyendo soluciones de escritorio, aplicaciones web, servicios API REST y proyectos que interactúan con servicios externos.",
+    "about.projects":
+      "A continuación, puedes encontrar información detallada sobre algunas de mis habilidades y sobre los proyectos en los que he participado.",
+
+    "experience.title": "Experiencia",
+    "experience.smurfit.company": "Smurfit Kappa Iberoamericana SL",
+    "experience.smurfit.duration": "jun 2022 - dic 2022",
+    "experience.smurfit.description":
+      "Soporte IT a la fábrica. Redes y shell scripting.",
+    "experience.akra.company": "Akra Informática Coop V",
+    "experience.akra.duration": "ene 2023 - actualidad",
+    "experience.akra.description": "Desarrollo de aplicaciones.",
+
+    "projects.title": "Proyectos",
+    "projects.sgiweb.title": "SGIWeb",
+    "projects.sgiweb.description":
+      "Opera tu negocio con este ERP que ofrece una interfaz de usuario moderna y una experiencia de usuario fluida, dando soporte a la automatización y gestión para que las empresas puedan mantener un control riguroso sobre sus operaciones diarias.",
+    "projects.akraVeriFactu.title": "AkraVeriFactu",
+    "projects.akraVeriFactu.description":
+      "Facilita el cumplimiento de las normativas fiscales al proporcionar una plataforma para la gestión y presentación de documentos fiscales. AkraVeriFactu ofrece una solución robusta y fiable para las empresas que necesitan mantenerse al día con las regulaciones fiscales.",
+    "projects.akraFACe.title": "AkraFACe",
+    "projects.akraFACe.description":
+      "Gestiona segura y eficientemente tus facturas electrónicas. Facilitando la presentación de éstas a través de una aplicación integrada o desde el portal FACe.",
+    "projects.mailManager.title": "MailManager",
+    "projects.mailManager.description":
+      "Automatiza la gestión de correos electrónicos. Esta herramienta busca correos electrónicos no leídos en la bandeja de entrada para generar respuestas automáticas.",
+
+    'languages.title': 'Idiomas',
+    'languages.spanish.label': 'Español',
+    'languages.spanish.level': 'NATIVO',
+    'languages.english.label': 'Inglés',
+    'languages.english.level': 'B1',
+    'languages.valencian.label': 'Valenciano',
+    'languages.valencian.level': 'C1',
+
+    'technologies.title': 'Tecnologías',
+    
+    'footer.rights': '© 2024 Francisco Ferrández Martínez. Todos los derechos reservados.'
+  },
+} as const;
